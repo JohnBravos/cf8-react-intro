d363c64146f80162a190617bf6a4daba63ce806f
bravosgiannis <bravosgiannis@gmail.com>
2025-09-27 17:35:44 +0300

    feat: add ClassComponentWithState and FunctionalComponentWithState components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
+// import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
+
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import Card from "./components/Card.tsx";
+import Layout from "./components/Layout.tsx";
+// import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 
 function App() {
 
   return (
-    <>
-        <ViteIntro />
-    </>
+      <>
+          {/*<ViteIntro />*/}
+          {/*<ClassComponent />*/}
+          {/*<FunctionalComponent />*/}
+          {/*<ArrowFunctionalComponent />*/}
+          {/*<ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component With Props" />*/}
+          {/*<ArrowFunctionalComponentWithProps title="2nd title" />*/}
+          {/*<ArrowFunctionalComponentWithPropsType */}
+          {/*  title="Is a Arrow Functional Component With 2 Props"*/}
+          {/*  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."*/}
+          {/*/>*/}
+          {/*<Card title="Card">*/}
+          {/*    <ArrowFunctionalComponentWithPropsType*/}
+          {/*        title="Is a Arrow Functional Component with 2 Props"*/}
+          {/*            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."*/}
+          {/*        />*/}
+          {/*</Card>*/}
+          <Layout>
+              {/*<ArrowFunctionalComponentWithPropsType*/}
+              {/*  title="Is a Arrow Functional Component With 2 Props"*/}
+              {/*  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"*/}
+              {/*/>*/}
+              {/*<FunctionalComponentWithState />*/}
+              <ClassComponentWithState />
+
+          </Layout>
+      </>
   )
 }
 
